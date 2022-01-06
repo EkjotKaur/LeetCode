1168160b7f169a6e43ebf8fee0da37933dbd6aa5
Ekjot Kaur <66993173+EkjotKaur@users.noreply.github.com>
2022-01-06 14:40:55 +0530

    Added solution - LeetHub

---
+++ Delete N nodes after M nodes of a linked list - GFG/delete-n-nodes-after-m-nodes-of-a-linked-list.cpp
@@ -0,0 +1,132 @@
+// { Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while(node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert(int n1)
+{
+    int n,value;
+    n=n1;
+    struct Node *temp;
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp=start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp=temp->next;
+        }
+    }
+}
+
+ // } Driver Code Ends
+/*
+delete n nodes after m nodes
+  The input list will have at least one element  
+  Node is defined as 
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+*/
+class Solution
+{
+    public:
+    void linkdelete(struct Node  *head, int M, int N)
+    {
+        //Add code here  
+        if(!head || N==0) return;
+        if(M==0) 
+        {
+            head=NULL; return;
+        }
+        
+        struct Node *p=head, *q=NULL;
+        while(p)
+        {
+            int m=M,n=N;
+            while(p && m--)
+            {
+                q=p;
+                p=p->next;
+            }
+            // cout<<q->data<<" ";
+            while(p && n--)
+            {
+                q->next=p->next;
+                p=p->next;
+            }
+        }
+        
+        
+        // if(N==0) head=p;
+        
+    }
+};
+
+
+
+// { Driver Code Starts.
+int main()
+{
+    int t,n1;
+    cin>>t;
+    while (t--) {
+        cin>>n1;
+        int m,n;
+        cin>>m;
+        cin>>n;
+        insert(n1);
+        Solution ob;
+        ob.linkdelete(start,m,n);
+        printList(start);
+    }
+    
+    return 0;
+}
+  // } Driver Code Ends
