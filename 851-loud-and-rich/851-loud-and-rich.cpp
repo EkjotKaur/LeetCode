3bb574b244614346189e1a2f697cf56bc7bd9727
Ekjot Kaur <66993173+EkjotKaur@users.noreply.github.com>
2022-02-03 18:47:39 +0530

    Time: 172 ms (42.77%), Space: 47.1 MB (24.21%) - LeetHub

---
+++ 851-loud-and-rich/851-loud-and-rich.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    void dfs(vector<vector<int>>&G, vector<pair<int,int>>&V, int i, vector<int>& quiet)
+    {
+        V[i]={quiet[i],i};
+        for(auto u:G[i])
+        {
+            if(V[u].first==-1)
+                dfs(G, V, u, quiet);
+            pair<int,int>x=V[u];
+            if(x.first<V[i].first) V[i]=x;
+        }
+    }
+public:
+    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+        int m=richer.size(), n=quiet.size();
+        
+        vector<vector<int>>G(n);
+        for(auto i:richer)
+            G[i[1]].push_back(i[0]);
+        
+        vector<pair<int,int>>value(n, {-1,-1});
+        
+        for(int i=0; i<n; i++)
+        {
+            if(value[i].first==-1) dfs(G, value, i, quiet);
+        }
+        
+        vector<int>ans;
+        for(auto i:value)
+            ans.push_back(i.second);
+        return ans;
+    }
+};
