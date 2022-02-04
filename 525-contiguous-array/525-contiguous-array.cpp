af5193c002d9a76d365449cdd8bef4d9c71b3c7e
Ekjot Kaur <66993173+EkjotKaur@users.noreply.github.com>
2022-02-04 12:35:15 +0530

    Time: 194 ms (40.43%), Space: 83.9 MB (25.19%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -3,21 +3,16 @@ public:
     int findMaxLength(vector<int>& nums) {
         
         int ans=0, n=nums.size();
-        unordered_map<int,pair<int,int>>h;
+        unordered_map<int,int>h;
         int count = 0;
         for(int i=0; i<n; i++)
         {
             count = nums[i]==1 ? count+1 : count-1;
-            if(h.find(count)==h.end()) h[count]={i,-1};
-            else h[count].second=i;
+            if(h.find(count)==h.end()) h[count]=i;
+            else ans=max(ans, i-h[count]);
             if(count==0) ans=max(ans, i+1);
         }
         
-        for(auto i:h)
-        {
-            // cout<<i.first<<" "<<i.second.first<<" "<<i.second.second<<"\n";
-            if(i.second.second!=-1) ans=max(ans, i.second.second-i.second.first);
-        }
         return ans;
     } 
 };
