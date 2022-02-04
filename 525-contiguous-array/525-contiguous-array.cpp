3a88dbea4e5327f524f90084a316ba04f21791b6
Ekjot Kaur <66993173+EkjotKaur@users.noreply.github.com>
2022-02-04 12:36:18 +0530

    Time: 294 ms (7.62%), Space: 83.9 MB (25.19%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -5,12 +5,12 @@ public:
         int ans=0, n=nums.size();
         unordered_map<int,int>h;
         int count = 0;
+        h[0]=-1;
         for(int i=0; i<n; i++)
         {
             count = nums[i]==1 ? count+1 : count-1;
             if(h.find(count)==h.end()) h[count]=i;
             else ans=max(ans, i-h[count]);
-            if(count==0) ans=max(ans, i+1);
         }
         
         return ans;
