50223cf210fa2a491a14745f25ec2b3bd1de982d
Ekjot Kaur <66993173+EkjotKaur@users.noreply.github.com>
2022-03-04 21:18:30 +0530

    Time: 20 ms (44.56%), Space: 14.2 MB (49.87%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -5,36 +5,21 @@ public:
         if(poured>1) A[0][0]=poured-1;
         else A[0][0]=-poured;
         for(int i=1; i<=query_row; i++)
-        {
             for(int j=0; j<=i; j++)
             {
                 if(j==0)
-                {
                     A[i][j] = A[i-1][0]>0 ? A[i-1][0]/2 : 0;
-                }
-                else if(j==i)
-                {
-                    A[i][j] = A[i-1][j-1]>0 ? A[i-1][j-1]/2 : 0;
-
-                }
                 else
                 {
                     A[i][j] += A[i-1][j]>0 ? A[i-1][j]/2 : 0;
                     A[i][j] += A[i-1][j-1]>0 ? A[i-1][j-1]/2 : 0;
                 }
-                // if(A[i][j]==1) A[i][j]=-1;
                 if(A[i][j]>1) A[i][j]-=1;
                 else A[i][j]=-A[i][j];
                 
                 if(i==query_row && j==query_glass)
-                {
                     return A[i][j]>0 ? 1 : abs(A[i][j]);
-                }
-                // cout<<A[i][j]<<" ";
-                
-                
             }
-        }
         return A[query_row][query_glass]>0 ? 1 : -A[query_row][query_glass];
 
     }
