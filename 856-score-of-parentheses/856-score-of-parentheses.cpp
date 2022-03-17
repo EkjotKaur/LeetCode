178b7927d27d04c28be232d46894ac8a95b7aaaf
Ekjot Kaur <66993173+EkjotKaur@users.noreply.github.com>
2022-03-17 11:59:44 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (45.05%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -2,35 +2,24 @@ class Solution {
 public:
     int scoreOfParentheses(string s) {
      
-        int left=0,right=0,n=s.length(),side=0;
+        int n=s.length(), ans=0;
         stack<int>S;
         for(int i=0; i<n; i++)
         {
-            if(s[i]=='(')
-               S.push(-1);
+            if(s[i]=='(') S.push(-1);
             else
             {
-                if(S.top()==-1)
+                int x = 0;
+                while(!S.empty() && S.top()>=1)
                 {
+                    x+=S.top();
                     S.pop();
-                    S.push(1);
                 }
-                else
-                {
-                    int x = 0;
-                    while(!S.empty() && S.top()>=1)
-                    {
-                        x+=S.top();
-                        S.pop();
-                    }
-                    if(!S.empty()) S.pop();
-                    x*=2;
-                    S.push(x);
-                }
+                S.pop();
+                x=x>0?x*2:1;
+                S.push(x);
             }
-            // if(!S.empty()) cout<<S.top()<<" ";
         }
-        int ans=0;
         while(!S.empty())
         {
             ans+=S.top(); 
