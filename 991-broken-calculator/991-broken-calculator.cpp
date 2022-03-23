593fca02e25b89bd00fed996f9a7818181205ef6
Ekjot Kaur <66993173+EkjotKaur@users.noreply.github.com>
2022-03-23 19:30:38 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.31%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -3,13 +3,13 @@ public:
     int brokenCalc(int startValue, int target) {
         
         int step=0;
-        while(target!=startValue)
+        while(target>startValue)
         {
-            if(target<startValue || target&1) target++;
+            if(target&1) target++;
             else  target/=2;
             step++;
         }
         
-        return step;
+        return step+startValue-target;
     }
 };
